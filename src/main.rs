5eb810cc857ae15ee9beee1cf54f39cc0289af20
NavePnow <evan.wong.stu@gmail.com>
2020-11-06 13:37:08 +0800

    feat: execute sql statement function

--- src/main.rs
+++ src/main.rs
@@ -3,23 +3,25 @@ use postgres::{Client, NoTls, Error};
 fn connect_database(conn_string: String) -> postgres::Client {
    return Client::connect(&conn_string, NoTls).expect("Failed to connect database!,{}");
 }
-fn create_table(){
-
+fn execute_sql(client: &mut Client, sql_string: &str) -> u64{
+    return client.execute(sql_string, &[]).expect("Failed to execute SQL statements");
 }
 
 fn main() {
 
-   let conn_string = String::from("host=198.13.60.74 port=5433 dbname=wangyifan  user=root password=120912");
-   
-    // let mut client = connect_databse(&conn_string); //
-    let res = connect_databse(&conn_string);
-    // let mut client = Client::connect("host=198.13.60.74:5433 user=root", NoTls);
-    match res {
-        Ok(client) => {println!("Succeeded!");
-        let mut client1 = client;
-        
-        },
-        Err(e) => {println!("Error: {}!", e);}
-    }
+    let conn_string = String::from("host=198.13.60.74 port=5433 dbname=wangyifan  user=root password=120912");
+    let mut client = connect_database(conn_string);
+    // let sql_string = String::from("CREATE TABLE IF NOT EXISTS author (
+    //                 id              SERIAL PRIMARY KEY,
+    //                 name            VARCHAR NOT NULL,
+    //                 country         VARCHAR NOT NULL
+    //               )");
+    let sql_string = "CREATE TABLE IF NOT EXISTS author1 (
+                    id              SERIAL PRIMARY KEY,
+                    name            VARCHAR NOT NULL,
+                    country         VARCHAR NOT NULL
+                  )";
+    execute_sql(&mut client, sql_string);
+
 
 }
