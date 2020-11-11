04c270c7a245fac75346f89d99279a620c5e8716
NavePnow <evan.wong.stu@gmail.com>
2020-11-11 17:12:00 +0800

    feat: comment diesel

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,21 @@
-#[macro_use]
-extern crate diesel;
-extern crate dotenv;
+// #[macro_use]
+// extern crate diesel;
+// extern crate dotenv;
 
-use diesel::prelude::*;
-use diesel::pg::PgConnection;
-use dotenv::dotenv;
-use std::env;
+// use diesel::prelude::*;
+// use diesel::pg::PgConnection;
+// use dotenv::dotenv;
+// use std::env;
 
-pub fn establish_connection() -> PgConnection {
-    dotenv().ok();
+// pub fn establish_connection() -> PgConnection {
+//     dotenv().ok();
 
-    let database_url = env::var("DATABASE_URL")
-        .expect("DATABASE_URL must be set");
-    PgConnection::establish(&database_url)
-        .expect(&format!("Error connecting to {}", database_url));
-    diesel::sql_query("SELECT * FROM users WHERE id > & AND name <> ?")
-    .bind::<Integer, _>(1) // bind i8 to Integer
-    .bind::<Text, _>("Tess") // bind &str to Text
-    .get_results(&connection); // define &connection
-}
+//     let database_url = env::var("DATABASE_URL")
+//         .expect("DATABASE_URL must be set");
+//     PgConnection::establish(&database_url)
+//         .expect(&format!("Error connecting to {}", database_url));
+//     diesel::sql_query("SELECT * FROM users WHERE id > & AND name <> ?")
+//     .bind::<Integer, _>(1) // bind i8 to Integer
+//     .bind::<Text, _>("Tess") // bind &str to Text
+//     .get_results(&connection); // define &connection
+// }
