0e676e8e1a182547becfc9d015b375d623e83566
NavePnow <yifwang97@gmail.com>
2020-12-23 17:32:53 +0800

    feat: generate json from table

--- app.py
+++ app.py
@@ -41,6 +41,26 @@ def print_all_tree(tab=1):
         traverse_trees(sid=i.sid)
 
 
+def get_simple_json():
+    groups = db_session.execute(
+        "SELECT * FROM dbo.springtb").fetchall()
+    result = []
+    for i in groups:
+        if i.pid == None:
+            _dict = {
+                'id': i.sid,
+                'pId': 0,
+                'name': i.name,
+            }
+        else:
+            _dict = {
+                'id': i.sid,
+                'pId': i.pid,
+                'name': i.name,
+            }
+        result.append(_dict)
+    return result
+
 # 创建flask的应用对象
 # __name__表示当前的模块名称
 # 模块名: flask以这个模块所在的目录为根目录，默认这个目录中的static为静态目录，templates为模板目录
@@ -58,6 +78,6 @@ if __name__ == '__main__':
         'postgresql+psycopg2://postgres:postgres@198.13.60.74:5433/wangyifan')
     mptt_session = mptt_sessionmaker(sessionmaker(bind=engine))
     db_session = scoped_session(sessionmaker(bind=engine))
-    # print_all_tree()
+    # get_simple_json()
     # 启动flask
     app.run()
