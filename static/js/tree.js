2f91c552314e9e8956b5d2d0c342fc4f09513c5d
NavePnow <yifwang97@gmail.com>
2020-12-25 16:03:10 +0800

    feat: load node dynamically

--- static/js/tree.js
+++ static/js/tree.js
@@ -7,24 +7,55 @@ var setting = {
             enable: true,
         },
     },
-};
+    callback: {
+        // onClick: addSubNode,
+        onExpand: addSubNode
+    }
+}
+var zNodes;
+var zTree;
+
+function addSubNode(event, treeId, treeNode) {
+    if (!treeNode.isParent) return;
+    var s = treeNode.children;
+    if (s != undefined)
+        return;
+    console.log(treeNode.id)
+    $.ajax({
+        cache: true,
+        url: "getjson",
+        type: "GET",
+        dataType: "json",
+        data: { sId: treeNode.id },
+        async: true,
+        success: function (data) {
+            // var newNodes = [{ name: "newNode1" }, { name: "newNode2" }, { name: "newNode3" }];
+            console.log(data)
+            // console.log(newNodes)
+            zTree.addNodes(treeNode, data);
+        },
+        error: function (data) {
+            alert("error");
+        }
+    });
+
+}
 
 function onLoadTree() {
-    var zNodes;
     $.ajax({
+        cache: true,
         url: "getjson",
-        type: "POST",
+        type: "GET",
         dataType: "json",
+        async: false,
         success: function (data) {
             zNodes = data;
-            $(document).ready(function () {
-                $.fn.zTree.init($("#treeDemo"), setting, zNodes);
-            });
         },
         error: function () {
             console.log(error);
         },
     });
+    zTree = $.fn.zTree.init($("#treeDemo"), setting, zNodes);
 }
 
 $(function () {
