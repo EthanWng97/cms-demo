0fb37b88272fdde99420f94ba3d08541e65c8c7f
NavePnow <yifwang97@gmail.com>
2021-03-10 16:57:45 +0800

    feat: onRightClick function

--- static/js/tree_pure_async.js
+++ static/js/tree_pure_async.js
@@ -16,12 +16,13 @@ var tree = {
             dataFilter: dataFilter
         },
         callback: {
-            onExpand: expandNode
+            onExpand: expandNode,
+            onRightClick: onRightClick,
         }
     },
     preLoadNode: function (rawData) {
         if (!rawData) rawData = tree.zTree.getNodes();
-        for (var p in rawData) {//遍历json对象的每个key/value对,p为key
+        for (var p in rawData) { //遍历json对象的每个key/value对,p为key
             if (rawData[p].isParent == 1 && rawData[p].children == undefined) {
                 console.log("preload: " + rawData[p].name);
                 tree.zTree.reAsyncChildNodesPromise(rawData[p], "refresh", isSilent = true);
@@ -29,32 +30,15 @@ var tree = {
         }
     }
 }
-// var setting = {
-//     data: {
-//         simpleData: {
-//             enable: true,
-//         },
-//     },
-//     async: { // 属性配置
-//         enable: true,
-//         url: "getunionjson",
-//         otherParam: getParam,
-//         type: 'post',
-//         dataType: "json",
-//         dataFilter: dataFilter
-//     },
-//     callback: {
-//         onExpand: expandNode
-//     }
-// }
 var zNodes;
 var zTree;
-//获取树成功后进行的回调操作
 
 function getParam(treeId, treeNode) {
     var data_list = [];
     data_list.push(treeNode.id);
-    var jsonObj = { "sId": JSON.stringify(data_list) };
+    var jsonObj = {
+        "sId": JSON.stringify(data_list)
+    };
     return jsonObj
 }
 
@@ -63,38 +47,6 @@ function dataFilter(treeId, parentNode, responseData) {
         return responseData[parentNode.id];
     }
 }
-// function expandNode(event, treeId, treeNode) {
-//     if (!treeNode.isParent) return;
-//     var data = treeNode.children;
-//     preLoadNode(data);
-// }
-
-// function preLoadNode(rawData) {
-//     if (!rawData) rawData = zTree.getNodes();
-//     for (var p in rawData) {//遍历json对象的每个key/value对,p为key
-//         if (rawData[p].isParent == 1 && rawData[p].children == undefined) {
-//             console.log("preload: " + rawData[p].name);
-//             zTree.reAsyncChildNodesPromise(rawData[p], "refresh", isSilent = true);
-//         }
-//     }
-// }
-// function onLoadTree() {
-//     $.ajax({
-//         cache: true,
-//         url: "getunionjson",
-//         type: "POST",
-//         dataType: "json",
-//         async: true,
-//         success: function (data) {
-//             zNodes = data["0"];
-//             zTree = $.fn.zTree.init($("#treeDemo"), setting, zNodes);
-//             preLoadNode(zTree.getNodes());
-//         },
-//         error: function (error) {
-//             console.log(error);
-//         },
-//     });
-// }
 
 $(function () {
     onLoadTree();
