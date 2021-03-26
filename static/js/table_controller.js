442205cb8203d82fd6638d6717d7d7015f1b15bc
NavePnow <yifwang97@gmail.com>
2021-03-26 18:02:26 +0800

    feat: table template

--- static/js/table_controller.js
+++ static/js/table_controller.js
@@ -1,61 +1,45 @@
-function showTable() {
-    layui.use(['table'], function () {
-        var table = layui.table;
-        table.render({
-            elem: '#tableDemo' //指定原始表格元素选择器（推荐id选择器）
-                ,
-            height: 315 //容器高度
-                ,
-            cols: [
-                [ //表头
-                    {
-                        field: 'id',
-                        title: 'ID',
-                        width: 80,
-                        sort: true,
-                        fixed: 'left'
-                    }, {
-                        field: 'username',
-                        title: '用户名',
-                        width: 80
-                    }, {
-                        field: 'sex',
-                        title: '性别',
-                        width: 80,
-                        sort: true
-                    }, {
-                        field: 'city',
-                        title: '城市',
-                        width: 80
-                    }, {
-                        field: 'sign',
-                        title: '签名',
-                        width: 177
-                    }, {
-                        field: 'experience',
-                        title: '积分',
-                        width: 80,
-                        sort: true
-                    }, {
-                        field: 'score',
-                        title: '评分',
-                        width: 80,
-                        sort: true
-                    }, {
-                        field: 'classify',
-                        title: '职业',
-                        width: 80
-                    }, {
-                        field: 'wealth',
-                        title: '财富',
-                        width: 135,
-                        sort: true
-                    }
-                ]
-            ] //设置表头
-            //,…… //更多参数参考右侧目录：基本参数选项
-        });
+layui.use(['table'], function () {});
 
+function showTable(id) {
+    tableId = '#' + id;
+    layui.table.render({
+        elem: tableId //指定原始表格元素选择器（推荐id选择器）
+            ,
+        height: 600 //容器高度
+            ,
+        cols: cols_template,
+        data: data_template,
+            
+        even: true
+        //,page: true //是否显示分页
+        //,limits: [5, 7, 10]
+        ,limit: 20 //每页默认显示的数量
+        , done: function (res, curr, count) {
+            // $("[data-field='lowerHairPath']").css('display', 'none');
+            // $(".layui-table-main tr").each(function (index, val) {
+            //     $($(".layui-table-fixed-l .layui-table-body tbody tr")[index]).height($(val).height());
+            //     $($(".layui-table-fixed-r .layui-table-body tbody tr")[index]).height($(val).height());
+            // });
+            // //动态监听表头高度变化，冻结行跟着改变高度
+            // $(".layui-table-header tr").resize(function () {
+            //     $(".layui-table-header tr").each(function (index, val) {
+            //         $($(".layui-table-fixed .layui-table-header table tr")[index]).height($(val).height());
+            //     });
+            // });
+            // //初始化高度，使得冻结行表头高度一致
+            // $(".layui-table-header tr").each(function (index, val) {
+            //     $($(".layui-table-fixed .layui-table-header table tr")[index]).height($(val).height());
+            // });
+            // //动态监听表体高度变化，冻结行跟着改变高度
+            // $(".layui-table-body tr").resize(function () {
+            //     $(".layui-table-body tr").each(function (index, val) {
+            //         $($(".layui-table-fixed .layui-table-body table tr")[index]).height($(val).height());
+            //     });
+            // });
+            // //初始化高度，使得冻结行表体高度一致
+            // $(".layui-table-body tr").each(function (index, val) {
+            //     $($(".layui-table-fixed .layui-table-body table tr")[index]).height($(val).height());
+            // });
+        }
     });
-
 }
