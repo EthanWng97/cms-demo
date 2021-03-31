3b139001f1b667606d3d298db498cdd304499527
NavePnow <yifwang97@gmail.com>
2021-03-31 17:32:24 +0800

    feat: load select and checkbox dagta

--- static/js/table_controller.js
+++ static/js/table_controller.js
@@ -1,20 +1,63 @@
-layui.use(['table'], function () {});
+layui.use(['table', 'form'], function () {});
 
 function showTable(id) {
-    tableId = '#' + id;
+    var tableId = '#' + id;
+    var requestUrl = '/dataset/table/' + id
     layui.table.render({
-        elem: tableId //指定原始表格元素选择器（推荐id选择器）
-            ,
-        height: 600 //容器高度
-            ,
+
+        elem: tableId, //指定原始表格元素选择器（推荐id选择器）
+        url: requestUrl,
+        height: 600, //容器高度
         cols: cols_template,
         data: data_template,
-            
-        even: true
+
+        even: true,
         //,page: true //是否显示分页
         //,limits: [5, 7, 10]
-        ,limit: 20 //每页默认显示的数量
-        , done: function (res, curr, count) {
+
+        limit: 20, //每页默认显示的数量
+
+        done: function (res, curr, count) {
+            $.each(res.data, function (index, values, arr) {
+                console.log(values);
+
+                // values['isField'];
+                $('#isField-' + values['id']).prop("checked", values['isField'] == 1);
+
+                // values['fdType'];
+                $('#fdType-' + values['id']).val(values['fdType']);
+
+                // values['isNullable'];
+                $('#isNullable-' + values['id']).prop("checked", values['isNullable'] == 1);
+
+                // values['isUseable'];
+                $('#isUseable-' + values['id']).prop("checked", values['isUseable'] == 1);
+
+                // values['isForeignKey'];
+                $('#isForeignKey-' + values['id']).prop("checked", values['isForeignKey'] == 1);
+
+                // values['uiType'];
+                $('#uiType-' + values['id']).val(values['uiType'].toString());
+
+                // values['uiVisible'];
+                $('#uiVisible-' + values['id']).prop("checked", values['uiVisible'] == 1);
+
+                // values['uiReadOnly'];
+                $('#uiReadOnly-' + values['id']).prop("checked", values['uiReadOnly'] == 1);
+
+                // values['isAddField'];
+                $('#isAddField-' + values['id']).prop("checked", values['isAddField'] == 1);
+
+                // values['isEditField'];
+                $('#isEditField-' + values['id']).prop("checked", values['isEditField'] == 1);
+
+                // values['orderType'];
+                $('#orderType-' + values['id']).val(values['orderType'].toString());
+                
+                layui.form.render();
+            });
+            // $('#isFieldCheckbox').val(data['fieldno']);
+            //刷新select选择框渲染
             // $("[data-field='lowerHairPath']").css('display', 'none');
             // $(".layui-table-main tr").each(function (index, val) {
             //     $($(".layui-table-fixed-l .layui-table-body tbody tr")[index]).height($(val).height());
